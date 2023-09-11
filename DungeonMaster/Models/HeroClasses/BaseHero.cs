b254f85cf5a6e75552c95aac0a2ef0ca536cea0b
ZillaCoop <cooprix1@gmail.com>
2023-09-11 15:37:59 +0200

    Started project

---
+++ DungeonMaster/Models/HeroClasses/BaseHero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonMaster.Models.HeroClasses
+{
+    abstract class BaseHero
+    {
+        public string Name { get; set; }
+        public int Level { get; set; } = 1; //Sets default level to 1.
+
+    }
+}
